52725f4a6c859d1b3904eab2afe0c22e44a662c0
lfrankfrade <lfrankfrade@gmail.com>
2024-10-27 16:44:18 -0300

    Adicionadas novas funções

--- galeria/models.py
+++ galeria/models.py
@@ -1,13 +1,25 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
 
 #A classe fotografia representa cada uma das fotografias
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("GALÁXIA", "Galáxia"),
+        ("ESTRELA", "Estrela"),
+        ("PLANETA", "Planeta"),
+    ]
+    
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIA, default="")
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
