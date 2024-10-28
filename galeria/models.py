a26c3be3ba2cca639e876842b403239aa8dfa133
lfrankfrade <lfrankfrade@gmail.com>
2024-10-28 00:08:40 -0300

    adicionando novas funções

--- galeria/models.py
+++ galeria/models.py
@@ -12,14 +12,14 @@ class Fotografia(models.Model):
         ("ESTRELA", "Estrela"),
         ("PLANETA", "Planeta"),
     ]
-    
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIA, default="")
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f"Fotografia [nome={self.nome}]"
+        return self.nome
